77a9a3e6ddad0c887717fca204b6adccb9e8fea8
Nikita Tsukanov <keks9n@gmail.com>
2013-10-10 20:00:11 +0400

    Whoops, Event.Init still should be called manually from tests

--- Tests/TestBase.cs
+++ Tests/TestBase.cs
@@ -19,6 +19,8 @@ namespace Tests
 					return;
 				if (Path.DirectorySeparatorChar == '\\')
 					Event.Init(Directory.GetCurrentDirectory());
+				else
+					Event.Init();
 				_initialized = true;
 			}
 			
