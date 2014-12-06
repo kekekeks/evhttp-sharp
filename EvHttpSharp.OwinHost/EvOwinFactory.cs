51069028fd1e211eef38e8bd5de6a153e4bca256
Nikita Tsukanov <keks9n@gmail.com>
2014-12-07 03:09:07 +0400

    Cleanup

---
+++ EvHttpSharp.OwinHost/EvOwinFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EvHttpSharp.OwinHost
+{
+    static class EvOwinFactory
+    {
+
+        //HACK: dunno how to pass these correctly
+        [ThreadStatic]
+        private static string _host;
+
+        [ThreadStatic]
+        private static ushort _port;
+
+
+        public static void Initialize(IDictionary<string, object> properties)
+        {
+            if (properties == null)
+                throw new ArgumentNullException("properties");
+            properties["owin.Version"] = "1.0";
+        }
+
+
+
+        public static IDisposable Create(Func<IDictionary<string, object>, Task> app,
+            IDictionary<string, object> properties)
+        {
+            if (app == null)
+                throw new ArgumentNullException("app");
+            if (properties == null)
+                throw new ArgumentNullException("properties");
+
+            return new EvOwinListener(app, _host, _port);
+        }
+
+        public static void ThreadInit(string address, int port)
+        {
+            _host = address;
+            _port = (ushort)port;
+        }
+    }
+}
--- EvHttpSharp.OwinHost/EvOwinHost.cs
+++ EvHttpSharp.OwinHost/EvOwinHost.cs
@@ -9,54 +9,21 @@ using Owin;
 
 namespace EvHttpSharp.OwinHost
 {
-    public sealed class EvOwinHost : IDisposable
+    public static class EvOwinHost
     {
         public static IDisposable Start(string address, int port, Action<IAppBuilder> startup)
         {
             var opts = new StartOptions
             {
                 Port = port,
-                ServerFactory = "EvHttpSharp.OwinHost.EvOwinHost"
+                ServerFactory = "EvHttpSharp.OwinHost.EvOwinFactory"
             };
-            _host = address;
-            _port = (ushort) port;
+            EvOwinFactory.ThreadInit(address, port);
             return WebApp.Start(opts, startup);
 
         }
 
-        //HACK: dunno how to pass these correctly
-        [ThreadStatic] private static string _host;
-
-        [ThreadStatic] private static ushort _port;
-
-        public static void Initialize(IDictionary<string, object> properties)
-        {
-            if (properties == null)
-                throw new ArgumentNullException("properties");
-            properties["owin.Version"] = "1.0";
-        }
 
 
-
-        public static IDisposable Create(Func<IDictionary<string, object>, Task> app,
-            IDictionary<string, object> properties)
-        {
-            if (app == null)
-                throw new ArgumentNullException("app");
-            if (properties == null)
-                throw new ArgumentNullException("properties");
-
-            return new EvOwinListener(app, _host, _port);
-        }
-
-        private EvOwinHost()
-        {
-            
-        }
-
-        public void Dispose()
-        {
-           
-        }
     }
 }
